58d68fa75138095a5f67d2d06e70c7c7f419ad02
Gabriel <gfursten@amazon.com>
2020-04-17 16:47:40 +0200

    Allow additions to url

--- extension/background.js
+++ extension/background.js
@@ -1,20 +1,48 @@
 (function() {
 	'use strict';
 
-	chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-		var mappingKey = 'nl.sjmulder.urlrewrite.mappings';
-		var mappings = JSON.parse(localStorage[mappingKey] || '[]');
+	try {
+    chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
+      try {
+        var mappingKey = 'nl.sjmulder.urlrewrite.mappings';
+        var additionsKey = 'nl.sjmulder.urlrewrite.additions';
 
-		for (var i = 0; i < mappings.length; i++) {
-			var mapping = mappings[i];
-			if (details.url.indexOf(mapping.sourceUrl) == 0) {
-				var newUrl = mapping.destinationUrl + details.url.slice(mapping.sourceUrl.length);
-				// console.log('rewriting', details.url, 'to', newUrl);
-				
-				chrome.tabs.update(details.tabId, { url: newUrl })
-				break;
-			}
-		}
-	});
+        var mappings = JSON.parse(localStorage[mappingKey] || '[]');
+        var additions = JSON.parse(localStorage[additionsKey] || '[]');
+        var newUrl = details.url
+        for (var i = 0; i < mappings.length; i++) {
+          const mapping = mappings[i];
+          if (details.url.indexOf(mapping.sourceUrl) === 0) {
+            newUrl = mapping.destinationUrl + details.url.slice(mapping.sourceUrl.length);
+            console.log('rewriting', details.url, 'to', newUrl);
+            chrome.tabs.update(details.tabId, {url: newUrl})
+            break
+          }
+        }
+        for (var i = 0; i < additions.length; i++) {
+          const addition = additions[i]
+          if (newUrl.indexOf(addition.sourceUrl) === 0) {
+            const parsedUrl = new URL(newUrl)
+            if (parsedUrl.search && parsedUrl.search.indexOf(addition.addition) !== -1) {
+              continue
+            }
+            if (parsedUrl.search) {
+              parsedUrl.search += '&' + addition.addition
+            } else {
+              parsedUrl.search = '?' + addition.addition
+            }
+            newUrl = parsedUrl.toString()
+            chrome.tabs.update(details.tabId, {url: newUrl})
+            break
+          }
+        }
+      } catch (e) {
+        console.error('Error on processing url', e)
+      }
+
+    });
+  } catch (e) {
+	  console.error('Error on adding lister', e)
+  }
 
 })();
--- extension/options.js
+++ extension/options.js
@@ -5,14 +5,27 @@ var app = angular.module('urlrewrite-options', []);
 
 app.controller('OptionsCtrl', function($scope) {
 	var mappingKey = 'nl.sjmulder.urlrewrite.mappings';
+	var additionsKey = 'nl.sjmulder.urlrewrite.additions';
 
+	function last (array) {
+	  return array[array.length - 1]
+  }
 	$scope.lastMapping = function() {
-		return $scope.mappings[$scope.mappings.length - 1];
+		return last($scope.mappings)
+	}
+	$scope.lastAddition = function() {
+		return last($scope.additions)
 	}
 
 	$scope.removeAt = function(index) {
 		if (index >= 0 && index < $scope.mappings.length - 1) {
 			$scope.mappings.splice(index, 1);
+		}
+	}
+
+	$scope.removeAdditionAt = function(index) {
+		if (index >= 0 && index < $scope.additions.length - 1) {
+			$scope.additions.splice(index, 1);
 		}
 	}
 
@@ -22,14 +35,27 @@ app.controller('OptionsCtrl', function($scope) {
 		}
 	}, true);
 
+	$scope.$watch('additions', function(value) {
+		if (value) {
+			localStorage[additionsKey] = angular.toJson(value.slice(0, value.length - 1));
+		}
+	}, true);
+
 	$scope.$watch('lastMapping()', function(value) {
 		if (value && (value.sourceUrl || value.destinationUrl)) {
 			$scope.mappings.push({ sourceUrl: '', destinationUrl: '' });
 		}
 	}, true);
+  $scope.$watch('lastAddition()', function(value) {
+		if (value && (value.sourceUrl || value.addition)) {
+			$scope.additions.push({ sourceUrl: '', addition: '' });
+		}
+	}, true);
 
 	$scope.mappings = JSON.parse(localStorage[mappingKey] || '[]');
+	$scope.additions = JSON.parse(localStorage[additionsKey] || '[]');
 	$scope.mappings.push({ sourceUrl: '', destinationUrl: '' });
+	$scope.additions.push({ sourceUrl: '', addition: '' });
 });
 
 })();
